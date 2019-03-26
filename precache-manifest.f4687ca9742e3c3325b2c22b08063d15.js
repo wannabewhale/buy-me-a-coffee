24a445cbd2d2c84c69743d47515dbcff61cffa2f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-26 02:50:19 +0000

    Deploy bmac.vecha.in to github.com/vechain/buy-me-a-coffee.git:gh-pages

---
+++ precache-manifest.f4687ca9742e3c3325b2c22b08063d15.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b7f413d263e937ddaff",
+    "url": "/css/app.9da70d23.css"
+  },
+  {
+    "revision": "1b7f413d263e937ddaff",
+    "url": "/js/app.0c1d9e8b.js"
+  },
+  {
+    "revision": "06f740b9d7c645a3defe",
+    "url": "/js/chunk-vendors.8004263d.js"
+  },
+  {
+    "revision": "48f86954ea8be75b53a5e15469453e32",
+    "url": "/fonts/gloria-hallelujah-v10-latin-regular.48f86954.eot"
+  },
+  {
+    "revision": "f1a55f31a6cf9a4bd3a55d0cc26ae94d",
+    "url": "/fonts/gloria-hallelujah-v10-latin-regular.f1a55f31.woff2"
+  },
+  {
+    "revision": "35a67f869f50bac65058bc88f226ab5b",
+    "url": "/fonts/gloria-hallelujah-v10-latin-regular.35a67f86.woff"
+  },
+  {
+    "revision": "f2acb614e446c90a634adfdc703d0b8a",
+    "url": "/fonts/gloria-hallelujah-v10-latin-regular.f2acb614.ttf"
+  },
+  {
+    "revision": "a6ef11a25bbed71b2e187bdefebc03da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
